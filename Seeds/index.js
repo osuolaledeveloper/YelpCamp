33c3d0051f9a6ef3c80e3a78e3144bfd2bf4ee8e
osuolale <osuolalehezekiah@gmail.com>
2024-10-02 09:37:57 +0100

    basic crud operattion

--- Seeds/index.js
+++ Seeds/index.js
@@ -9,11 +9,7 @@ const Campground = require('../models/campground');
 
 
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const db = mongoose.connection;
 
@@ -32,28 +28,28 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
-            author: '5f5c330c2cd79d538f2c66d9',
+            // author: '5f5c330c2cd79d538f2c66d9',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
-            geometry: {
-                type: "Point",
-                coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude,
-                ]
-            },
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
-                },
-                {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
-                }
-            ]
+            // geometry: {
+            //     type: "Point",
+            //     coordinates: [
+            //         cities[random1000].longitude,
+            //         cities[random1000].latitude,
+            //     ]
+            // },
+            // images: [
+            //     {
+            //         url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+            //         filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+            //     },
+            //     {
+            //         url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+            //         filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+            //     }
+            // ]
         })
         await camp.save();
     }
